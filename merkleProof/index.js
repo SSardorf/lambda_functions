5a3b3a0c1a011deab4d79fc878e422a359e9daf2
Simon Sardorf <SSardorf@gmail.com>
2022-03-05 09:46:46 +0100

    Add boolean return signifying whether user is verified or not

--- merkleProof/index.js
+++ merkleProof/index.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 const { MerkleTree } = require("merkletreejs");
 const keccak256 = require("keccak256");
 console.log("Loading MerkleTree");
 exports.handler = async (event) => {
-    let address = ''
+    let address = "";
     console.log("request: " + JSON.stringify(event));
     let body = JSON.parse(event.body);
     if (body.address) {
@@ -11,9 +11,9 @@ exports.handler = async (event) => {
     }
 
     console.log(address);
-    let proof = MerkleTreeProof(address);
+    let merkleResults = MerkleTreeProof(address);
     let responseBody = {
-        proof: proof,
+        merkleResults,
     };
 
     const response = {
@@ -35,14 +35,17 @@ function MerkleTreeProof(address) {
         "0x136BBBB19dF52AEe9dC5eF47de19E37eEA05b7e3",
         "0x83332BCDc4f6AE70e1a6F7f933Df0b9671b610Bf",
         "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
+        "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
     ];
 
     const leaves = addresses.map((address) => keccak256(address));
     const tree = new MerkleTree(leaves, keccak256, {
         sortPairs: true,
     });
-    //const root = tree.getRoot().toString("hex"); //TODO: Use this when you need to get the root of the tree (For the smart contract)
+    const root = tree.getRoot().toString("hex"); //TODO: Use this when you need to get the root of the tree (For the smart contract)
     let leaf = keccak256(address);
     const hexProof = tree.getHexProof(leaf);
-    return hexProof;
+    // Verify that address is part of the tree
+    let verified = tree.verify(hexProof, leaf, root);
+    return { proof: hexProof, verified: verified };
 }
